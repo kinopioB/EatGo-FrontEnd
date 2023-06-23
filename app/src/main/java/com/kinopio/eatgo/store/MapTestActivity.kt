c7bae5e9fbf873dfb046cf0223fde76760a63336
SeungJoo❤️ <seoungjoo01@gmail.com>
2023-06-23 11:03:30 +0900

    Initial Setting

---
+++ app/src/main/java/com/kinopio/eatgo/store/MapTestActivity.kt
@@ -0,0 +1,19 @@
+package com.kinopio.eatgo.store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import net.daum.mf.map.api.MapView
+
+class MapTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.kinopio.eatgo.R.layout.activity_map_test)
+        val mapView = MapView(this)
+
+        val mapViewContainer = findViewById<View>(com.kinopio.eatgo.R.id.map_view) as ViewGroup
+        mapViewContainer.addView(mapView)
+
+    }
+}
