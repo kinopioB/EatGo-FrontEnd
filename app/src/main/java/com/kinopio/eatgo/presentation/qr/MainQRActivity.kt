83dd9c4c4d3c3135da99b5e41b020ed044651aca
Yang_Seok_Jin <62278686+black-kit@users.noreply.github.com>
2023-06-28 02:42:59 +0900

    feat: 리뷰 팝업 창 기능 구현
    레이아웃 및 이벤트 처리

--- app/src/main/java/com/kinopio/eatgo/presentation/qr/MainQRActivity.kt
+++ app/src/main/java/com/kinopio/eatgo/presentation/qr/MainQRActivity.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.kinopio.eatgo.R
 import com.kinopio.eatgo.databinding.ActivityQrBinding
+import com.kinopio.eatgo.presentation.store.ReviewFragment
 
 class MainQRActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityQrBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityQrBinding.inflate(layoutInflater)
@@ -22,6 +26,11 @@ class MainQRActivity : AppCompatActivity() {
             Log.d("QR", "QR코드 인식")
             val intent = Intent( this, ScanQRActivity::class.java )
             startActivity(intent)
+        }
+        binding.btnReview.setOnClickListener {
+            ReviewFragment().show(
+                supportFragmentManager, "Review"
+            )
         }
     }
 }
---
+++ app/src/main/java/com/kinopio/eatgo/presentation/store/ReviewFragment.kt
@@ -0,0 +1,55 @@
+package com.kinopio.eatgo.presentation.store
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.kinopio.eatgo.R
+import com.kinopio.eatgo.databinding.FragmentReviewBinding
+
+class ReviewFragment : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //false로 설정해 주면 화면밖 혹은 뒤로가기 버튼시 다이얼로그라 dismiss 되지 않는다.
+        isCancelable = false
+
+    }
+
+    private lateinit var binding: FragmentReviewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentReviewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        binding.btnCancel.setOnClickListener {
+            dismiss()
+        }
+
+        binding.btnRegister.setOnClickListener{
+            // 별 값 가져오기
+            val ratingStar = binding.ratingStar.rating
+            // editText 값 가져오기
+            val inputText = binding.reviewText.text.toString()
+            Log.d("review", "editText : $inputText")
+            Log.d("review", "ratingStar : $ratingStar")
+
+        }
+    }
+}
