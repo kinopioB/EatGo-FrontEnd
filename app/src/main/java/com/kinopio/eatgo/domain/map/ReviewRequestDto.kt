4188debf129a4b90408b2722c13288a6cfaacaf6
SeungJoo🍎 <seoungjoo01@gmail.com>
2023-07-04 11:01:20 +0900

    feat: 가게 상세 페이지 데이터 띄우기 1차 구현 완료
    issue: #26

---
+++ app/src/main/java/com/kinopio/eatgo/domain/map/ReviewRequestDto.kt
@@ -0,0 +1,11 @@
+package com.kinopio.eatgo.domain.map
+
+import com.google.gson.annotations.SerializedName
+
+data class ReviewResponseDto (
+    val userId:Int,
+    val storeId:Int,
+    val userName : String,
+    val content:String,
+    val rating:Int
+)
